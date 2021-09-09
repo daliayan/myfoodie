92ba0ddf6042a1fa7529490ea0df9ce74805c8ba
daliayan <daliayan5@gmail.com>
2021-09-08 20:12:11 -0400

    Removed filler data - bad code

--- frontend/models/city.js
+++ frontend/models/city.js
@@ -20,53 +20,14 @@ class City {
         selectCityOption.innerText = this.name;
         selectCityDropdown.append(selectCityOption);
 
-
-        // const cityFiltDrop = document.getElementById("cityDropdown");
-        // // const filterDrop = document.createElement('div');
-        // // filterDrop.classList.add('filter-button');
-        // // filterDrop.id = this.id;
-        // // cityFiltDrop.appendChild(filterDrop);
-
-        // cityFiltDrop.addEventListener('change', () => {
-        //     // this.id = id;
-        //     console.log(this.name)
-        
-        // });
-
         cityDiv.appendChild(cityInfo);
         cityList.appendChild(cityDiv);
         cityList.appendChild(restaurants);
     };
 
-    // getDropDown(){
-    //     // const cityFiltDrop = document.getElementById("cityRestaurant");
-    //     const filterDrop = document.createElement('div');
-    //     filterDrop.classList.add('filter-button');
-    //     filterDrop.id = this.id;
-    //     selectCityDropdown.appendChild(filterDrop);
-
-    //     filterDrop.addEventListener('change', () => {
-    //         // this.id = id;
-    //         console.log(this.name)
-    // });
-    // }
-
     getCityInfo(){
         const cityData = document.createElement('div');
-        // cityData.id = this.id;
-        
         cityData.innerHTML = `${this.name}`; 
-
-        // const filterDrop = document.createElement('div');
-        // filterDrop.classList.add('filter-button');
-
-        // filterDrop.addEventListener('click', () => {
-        //     // this.id = id;
-        //     console.log(this.name)
-        // });
-
-        // cityData.append(filterDrop);
-
         return cityData;
     }
 
